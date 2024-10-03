de5c8784236217d7dc584a562dfa2456b189b514
Epric096 <156468920+Epric096@users.noreply.github.com>
2024-10-03 11:03:41 -0400

    More Folder
    added more folder for art and music, updated enemy damage script, can choose damage type now.

--- CosmicCrustCrusaders/Assets/Scripts/Enemies/Frog.cs
+++ CosmicCrustCrusaders/Assets/Scripts/Enemies/Frog.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Frog : MonoBehaviour
 {
+    private Transform forglocation;
+
     // Start is called before the first frame update
     void Start()
     {
--- CosmicCrustCrusaders/Assets/Scripts/Enemies/eDamage.cs
+++ CosmicCrustCrusaders/Assets/Scripts/Enemies/eDamage.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class eDamage : MonoBehaviour
 {
-    // Set up player health, needed to drag everytime, after use the code 
+    // Set up player health, needed to drag and drop everytime, after use the code 
     public pHealth playerHp;
     // Field to change the damage in editor
     [SerializeField]
     float damage;
+    // Clickable box for enemies damage type
+    [SerializeField]
+    bool onEnter;
+    [SerializeField]
+    bool onStay;
 
+    //damage the player when staying with enemies
     private void OnCollisionStay2D(Collision2D collision)
     {
-        // change this after player is ready, this is just an example
-        if (collision.gameObject.CompareTag("Player"))
+        if (onStay)
         {
-            playerHp.health -= damage;
-            // next line of code used in actual game, the one above just for testing for now
-            // other.gameObject.GetComponent<playerHealth>().health -= damage;
+            // change this after player is ready, this is just an example
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                playerHp.health -= damage;
+                // next line of code used in actual game, the one above just for testing for now, doesn't require drag and drop everytime
+                // other.gameObject.GetComponent<playerHealth>().health -= damage;
+            }
         }
     }
 
+    // damage the player when entering the enemy
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (onEnter)
+        {
+            // change this after player is ready, this is just an example
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                playerHp.health -= damage;
+                // next line of code used in actual game, the one above just for testing for now, doesn't require drag and drop everytime
+                // other.gameObject.GetComponent<playerHealth>().health -= damage;
+            }
+        }
+    }
 
 }
