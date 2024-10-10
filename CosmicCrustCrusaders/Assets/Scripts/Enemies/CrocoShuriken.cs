e3d02169770e0bc4d34b487618d7b43f48e091e6
Epric096 <156468920+Epric096@users.noreply.github.com>
2024-10-10 11:48:56 -0400

    Updated Crocodile Script
    updated shooting, and destroy shuriken

--- CosmicCrustCrusaders/Assets/Scripts/Enemies/CrocoShuriken.cs
+++ CosmicCrustCrusaders/Assets/Scripts/Enemies/CrocoShuriken.cs
@@ -4,29 +4,22 @@ using UnityEngine;
 
 public class CrocoShuriken : MonoBehaviour
 {
-    public Rigidbody2D rb;
-
     [SerializeField]
     private float damage;
-    [SerializeField]
-    private float flySpeed;
-    [SerializeField]
-    private float flyHeight;
 
     private void Start()
     {
-        rb = gameObject.AddComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(flySpeed, flyHeight);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "ground")
         {
-            Destroy (this);
+            Destroy (gameObject);
         }
         else if(collision.gameObject.tag == "Player")
         {
             collision.gameObject.GetComponent<pHealth>().health -= damage;
+            Destroy (gameObject);
         }
     }
 }
---
+++ CosmicCrustCrusaders/Assets/Scripts/Enemies/Crocodiles.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crocodiles : MonoBehaviour
+{
+    public GameObject shuriken;
+    [SerializeField]
+    private Transform shootingLocation;
+    [SerializeField]
+    private float shurikenSpeed;
+    [SerializeField]
+    private float shootAmout;
+    [SerializeField]
+    private float shootDelayTime;
+    [SerializeField]
+    private float fireRate;
+    private float shootTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootTime += Time.deltaTime;
+        if (shootTime > shootDelayTime)
+        {
+            for (int i = 0; i < shootAmout; i++)
+            {
+                shoot();
+            }
+            shootTime = 0;
+        }
+    }
+
+    private void shoot()
+    {
+        GameObject crocodileShoot = Instantiate(shuriken, shootingLocation.position, shootingLocation.rotation);
+        crocodileShoot.gameObject.GetComponent<Rigidbody2D>().AddForce(crocodileShoot.transform.right * shurikenSpeed);
+    }
+}
