0bf6e1e22045df6895d976b83c7b1799df800a28
Epric096 <156468920+Epric096@users.noreply.github.com>
2024-10-23 15:12:30 -0400

    added flower icon
    nothing special, didn't add code

---
+++ CosmicCrustCrusaders/Assets/Scripts/Enemies/Flower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    private float FBDistance; // flower to base
+    private float FPDistance; // flower to player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
