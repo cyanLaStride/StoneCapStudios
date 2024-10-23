c8d860ea528b571eb294e59c6259e84d4ebdc1b7
Epric096 <156468920+Epric096@users.noreply.github.com>
2024-10-23 15:03:01 -0400

    added cave spike
    added cave spike falling down (change the speed later)

---
+++ CosmicCrustCrusaders/Assets/Scripts/Enemies/FallingSpike.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpike : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            rb.gravityScale += Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.CompareTag("ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
