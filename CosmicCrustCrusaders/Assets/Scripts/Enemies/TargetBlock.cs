4da9ae6df9c5c0fa4bce4cea557120064e443cb3
Epric096 <156468920+Epric096@users.noreply.github.com>
2024-11-06 19:37:41 -0500

    added timer and target block

---
+++ CosmicCrustCrusaders/Assets/Scripts/Enemies/TargetBlock.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBlock : MonoBehaviour
+{
+    private Renderer ren;
+    [SerializeField]
+    private Color changeColor;
+    private Color orignalColor;
+    private float timer;
+    private float maxTime = 3f;
+    [SerializeField]
+    private bool isChanged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ren = GetComponent<Renderer>();
+        isChanged = false;
+        orignalColor = ren.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isChanged)
+        {
+            timer += Time.deltaTime;
+            if (timer >= maxTime)
+            {
+                ren.material.color = orignalColor;
+                isChanged = false;
+                timer = 0;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision != null)
+        {
+            ren.material.color = changeColor;
+            isChanged = true;
+        }
+    }
+}
--- CosmicCrustCrusaders/Assets/Scripts/GameManager.cs
+++ CosmicCrustCrusaders/Assets/Scripts/GameManager.cs
@@ -10,6 +10,12 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private TMP_Text Coins;
     public int CoinCount;
+
+    // timer
+    [SerializeField]
+    private TMP_Text timerText;
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +26,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         Coins.text = "Coins: " + CoinCount;
+        timer += Time.deltaTime;
+        timerText.text = "Timer: " + (int)timer;
     }
 }
