245274f47558ef89a12a0379e1002aa0769b735d
Epric096 <156468920+Epric096@users.noreply.github.com>
2024-11-26 23:58:27 -0500

    Adding more idea
    nothing special

--- CosmicCrustCrusaders/Assets/Scripts/Enemies/LavaSlimes.cs
+++ CosmicCrustCrusaders/Assets/Scripts/Enemies/LavaSlimes.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class LavaSlimes : MonoBehaviour
 {
+    // player and checking
+    private bool checkSameLevel;
+    [SerializeField]
+    private PlayerController player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        checkSameLevel = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.y == transform.position.y)
+        {
+            checkSameLevel = true;
+        }
     }
 }
