eff28f68abe81f152ccb1aaf82a4628e3e5d5c54
Epric096 <156468920+Epric096@users.noreply.github.com>
2025-02-20 00:06:16 -0500

    fixing

--- CosmicCrustCrusaders/Assets/Scripts/Enemies/Penguin.cs
+++ CosmicCrustCrusaders/Assets/Scripts/Enemies/Penguin.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class Penguin : MonoBehaviour
 {
+    [SerializeField]
+    private bool isRight;
+    private int direction;
+    [SerializeField]
+    private float force;
+
+    [SerializeField]
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     // I don't get it
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        if (isRight)
+        {
+            direction = 1;
+        }
+        else if (!isRight)
+        {
+            direction = -1;
+            transform.Rotate(new Vector3(0, 180, 0));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        rb.AddForce(transform.right * direction * force);   
     }
 }
