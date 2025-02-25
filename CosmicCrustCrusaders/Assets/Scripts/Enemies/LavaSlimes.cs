b7af067d44572b4f6cf99289355f6aa29ef41ff8
Epric096 <156468920+Epric096@users.noreply.github.com>
2025-02-25 15:50:31 -0500

    updated lava slime

--- CosmicCrustCrusaders/Assets/Scripts/Enemies/LavaSlimes.cs
+++ CosmicCrustCrusaders/Assets/Scripts/Enemies/LavaSlimes.cs
@@ -5,38 +5,48 @@ using UnityEngine;
 public class LavaSlimes : MonoBehaviour
 {
     // player and checking
-    private bool checkSameLevel;
+    //private bool checkSameLevel;
 
     [SerializeField]
     private PlayerController player;
     [SerializeField]
     private Rigidbody2D rb;
+
+    [SerializeField]
+    private float speed;
+    private int direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        checkSameLevel = false;
+        //checkSameLevel = false;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (player.transform.position.y - 0.3 >= transform.position.y - 0.3 && player.transform.position.y - 0.3 <= transform.position.y + 0.3)
         {
-            checkSameLevel = true;
+            //checkSameLevel = true;
+            transform.localScale = new Vector2(1.5f, 1.5f);
+            rb.velocity = Vector2.right * speed * 100 * direction * Time.fixedDeltaTime;
+
         }
         else if (player.transform.position.y -0.3 < transform.position.y - 0.4 || player.transform.position.y - 0.3 > transform.position.y + 0.4)
         {
-            checkSameLevel = false;
+            //checkSameLevel = false;
+            transform.localScale = new Vector2(1f, 1f);
+
         }
         //Debug.Log(checkSameLevel);
-        if (checkSameLevel)
+        if (player.transform.position.x > transform.position.x)
         {
-            transform.localScale = new Vector2(1.5f, 1.5f);
+            direction = 1;
         }
-        else if (!checkSameLevel)
+        else if (player.transform.position.x < transform.position.x)
         {
-            transform.localScale = new Vector2(1f, 1f);
+            direction = -1;
         }
     }
 }
