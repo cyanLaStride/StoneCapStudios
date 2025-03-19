60f549e1896f95707e9a5d714e024fa2d42876ca
brandonrpatel <bp21hq@brocku.ca>
2025-03-19 17:40:49 -0400

    Started Save Script

---
+++ CosmicCrustCrusaders/Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public void SaveData()
+    {
+        PlayerPrefs.SetString("Input" , inputField.text);
+        PlayerPrefs.SetString("PlayerHealth", inputField.text);
+        PlayerPrefs.SetString("Coins", inputField.text);
+        PlayerPrefs.SetString("Deaths" , inputField.text);
+        PlayerPrefs.SetString("Upgrades" , inputField.text);
+
+    }
+
+    public void LoadData()
+    {
+        inputField.text = PlayerPrefs.GetString("Input");    
+    }
+
+    public void DeleteData() 
+    {
+
+        PlayerPrefs.DeleteKey("Input");
+        PlayerPrefs.DeleteAll();
+    }
+}
